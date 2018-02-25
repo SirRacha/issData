c823b4c1abd44c304426cc1835316e5440c15f3e
SirRacha <31993629+SirRacha@users.noreply.github.com>
2018-02-25 09:35:03 -0600

    Delete old file

--- project002.py
+++
@@ -1,9 +0,0 @@
-from requests import get as geturl
-url = 'http://api.open-notify.org/iss-now.json'
-response = geturl(url)
-result = response.json()
-print(result)
-
-git commit
-
-This is a new file
